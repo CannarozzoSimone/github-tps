833aaa72c7c32bda9370933355e515561694575b
unknown <s.cannarozzo.3925@vallauri.net>
2026-02-05 12:26:00 +0100

    Commit laboratirio

--- QuizShow/index.js
+++ QuizShow/index.js
@@ -1,29 +1,53 @@
 var _domanda = document.getElementById("domanda");
 var _risposte = document.getElementsByTagName("span");
-var m=0;
+var _pallini = document.getElementsByName("risposta");
+
+var m = 0;
+var punti = 0;
 
 var domanda = ["Quanto fa 5+3?", "Capitale d'Italia", "JS è un linguaggio interpretato"];
-let risposte = [
+var risposte = [
     ["6", "7", "8", "9"],
     ["Napoli", "Torino", "Roma", "Fossano"],
     ["Si", "No", "Dipende", "Chiedo a Teo"]
 ];
-caricaDomanda();
-function conferma()
-{
-    if(m>=2){
+var soluzioni = [2, 2, 0];
 
-    }else{
-        m++;
-        caricaDomanda();
+caricaDomanda();
+
+function caricaDomanda() {
+    _domanda.textContent = domanda[m];
+    for (var j = 0; j < _risposte.length; j++) {
+        _risposte[j].textContent = risposte[m][j];
+        _pallini[j].checked = false;
     }
 }
-function caricaDomanda(){
 
-_domanda.textContent = domanda[m];
-for (let j = 0; j < _risposte.length; j++) {
-    if (risposte[m][j]) {
-        _risposte[j].textContent = risposte[m][j];
+function conferma() {
+    var scelta = -1;
+
+    for (var i = 0; i < _pallini.length; i++) {
+        if (_pallini[i].checked) {
+            scelta = i;
         }
+    }
+
+    if (scelta == -1) {
+        alert("Seleziona una risposta!");
+        return;
+    }
+
+    if (scelta == soluzioni[m]) {
+        alert("Corretto!");
+        punti++;
+    } else {
+        alert("Sbagliato!");
+    }
+
+    if (m < domanda.length - 1) {
+        m++;
+        caricaDomanda();
+    } else {
+        alert("Fine! punteggio finale: " + punti + "/" + domanda.length);
     }
 }
